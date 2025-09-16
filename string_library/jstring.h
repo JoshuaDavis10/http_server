73efcaa59ed3f660cef65be2be45624368a127b2
JoshuaDavis10 <jjdavis5760@icloud.com>
2025-09-16 03:49:27 +0000

    string library: added a way for user to load a custom logging function for library to log to, added static assertions and a janky assertiongot memory activation function setup

--- string_library/jstring.h
+++ string_library/jstring.h
@@ -1,14 +1,63 @@
 #ifndef JSTRING_H
 #define JSTRING_H
 
-#define SIZEOF_SIGNED_CHAR 1 
-#define SIZEOF_UNSIGNED_CHAR 1 
-#define SIZEOF_SIGNED_SHORT 2
-#define SIZEOF_UNSIGNED_SHORT 2
-#define SIZEOF_SIGNED_INT 4 
-#define SIZEOF_UNSIGNED_INT 4 
-#define SIZEOF_SIGNED_LONG_LONG 8 
-#define SIZEOF_UNSIGNED_LONG_LONG 8 
+static void (*jstring_log)(const char*, ...);
+
+/* TODO: this assertion sets off the address sanitizer, so I'll have to find a way
+ * around that in order to actually see the assertion message
+ */
+#define JSTRING_ASSERT(expression, msg) \
+if(!expression) \
+{ \
+	char tmp = *((char*)(0)); \
+	jstring_log(msg); \
+}
+
+
+/* NOTE: this is how you do a static assert without having to include C standard headers and
+ * stuff so this is how we doin' it 
+ */
+#define JSTRING_STATIC_ASSERT(expression, message) \
+typedef char static_assertion_##message[(expression)?1:-1]
+
+JSTRING_STATIC_ASSERT(sizeof(signed char) == 1, asserting_signed_char_size_is_one_byte);
+JSTRING_STATIC_ASSERT(sizeof(signed short) == 2, asserting_signed_short_size_is_two_bytes);
+JSTRING_STATIC_ASSERT(sizeof(signed int) == 4, asserting_signed_int_size_is_four_bytes);
+JSTRING_STATIC_ASSERT(sizeof(signed long long) == 8, 
+	asserting_signed_long_long_size_is_eight_bytes);
+JSTRING_STATIC_ASSERT(sizeof(unsigned char) == 1, asserting_unsigned_char_size_is_one_byte);
+JSTRING_STATIC_ASSERT(sizeof(unsigned short) == 2, asserting_unsigned_short_size_is_two_bytes);
+JSTRING_STATIC_ASSERT(sizeof(unsigned int) == 4, asserting_unsigned_int_size_is_four_bytes);
+JSTRING_STATIC_ASSERT(sizeof(unsigned long long) == 8, 
+		asserting_unsigned_long_long_size_is_eight_bytes);
+JSTRING_STATIC_ASSERT(sizeof(float) == 4, asserting_float_size_is_four_bytes);
+JSTRING_STATIC_ASSERT(sizeof(double) == 8, asserting_double_size_is_eight_bytes);
+
+typedef signed char i8;
+typedef signed short i16;
+typedef signed int i32;
+typedef signed long long i64;
+typedef unsigned char u8;
+typedef unsigned char b8; /* for "boolean" stuff, this is more so I can remember what stuff is
+						   * is used for booleans
+						   */
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long long u64;
+typedef float f32;
+typedef double f64;
+
+#define true 1
+#define false 0 
+
+/* NOTE: if I want to do logging, I had an idea for that:
+ * HAVE THE USER PASS function pointers to a logging function that they write?
+ * as long as the function they write takes in a format string and variadic args it
+ * works? then they can kinda take it from there as far as how they want to print the messages
+ * or better yet just turn logging off!
+ */
+static void jstring_log_stub(const char*, ...) { JSTRING_ASSERT(0, "using stub for logging"); }
+static void (*jstring_log)(const char*, ...) = jstring_log_stub;
     
 /* NOTE: goal is to have a header only string library that can compile
  * to c89 with 0 dependencies (including C standard library)
@@ -54,7 +103,79 @@
  * use more than that PERIOD
  */
 
-/* TODO: need some construct for what a string is */
+typedef struct {
+	u64 size;
+
+	/* current offset past address that we have allocated to */
+	u64 offset; 
+	void *address;
+	b8 activated;
+} jstring_memory;
+
+/* NOTE: could have like a free-list type thing, where if strings get "freed" we flag that
+ * so that we can fill those empty "blocks" later...
+ * simpler would be a linear allocator ? 
+ * maybe there's 2 types of strings - permanent, and temporary
+ * we have a function that wipes temporary? and temporary is a bump allocator
+ * permanent also a bump allocator that gets freed at the end... ?
+ * that seems like it covers most use cases as long as there's enough storage for "permanent" 
+ * strings. Note also the way the library is setup, permanent strings should be able to be
+ * stored in place -> WAIT PROBLEM THO, if we add to strings, concatenate, insert etc... how
+ * do we account for that?
+ * okay, this is an interesting problem space...
+ * maybe we do need like a first-fit free list (or red-black tree for efficiency <- more likely)
+ * that allocates double the string size to leave room and then like moves strings if it has to?
+ * - I just feel like the more can be done to strings IN PLACE the better
+ */ 
+
+/* NOTE: for now I'm going to do just a temporary store that we can clear periodically
+ * or whatever to get the functions up and running because I feel like writing code that 
+ * does stuff rather than thinking a whole bunch. And I feel like the string processing
+ * problem space needs to be fleshed out more in my head before I can make decisions about
+ * how to manage the memory around it... does that make sense? 
+ */
+
+typedef struct {
+	u32 length;
+	char *data;
+} jstring;
+
+static jstring_memory jstring_temporary_memory_info = {0};
+
+/* NOTE: basically user is just saying, "hey you get to use this memory, I will not touch it" */
+static b8 jstring_memory_activate(u64 size, void *address)
+{
+	if(jstring_temporary_memory_info.activated)
+	{
+		return false;
+	}
+	jstring_temporary_memory_info.size = size;
+	jstring_temporary_memory_info.offset = 0;
+	jstring_temporary_memory_info.address = address;
+	jstring_temporary_memory_info.activated = true;
+
+	jstring_log("jstring: test log %d", 0);
+	return true;
+}
+
+static b8 jstring_load_logging_function(void (*func)(const char*, ...))
+{
+	if(func == 0)
+	{
+		/* TODO: is there any way to log this failure lol */
+		/* probably just document that this is the only failure case */
+		return false;
+	}
+	jstring_log = func;
+	/* NOTE: I was gonna have it return false if we already have loaded a logging function
+	 * but honestly who cares, load new logging functions mid program i don't care lol
+	 */
+	return true;
+}
+
 /* TODO: ITERATE! start with smn stupid and explore the problem space */
+/* NOTE: might be important to have versions of functions that can also just pass a char *
+ * so that they don't have to use our string type
+ */ 
 
 #endif
--- string_library/test.c
+++ string_library/test.c
@@ -1,11 +1,25 @@
 #include "jstring.h"
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+#define MAX_LOGGER_MESSAGE_SIZE 1024
+
+void LOG_WARN(const char *message, ...) {
+	char output[MAX_LOGGER_MESSAGE_SIZE];
+	va_list arg_ptr;
+	va_start(arg_ptr, message);
+    vsnprintf(output, MAX_LOGGER_MESSAGE_SIZE, message, arg_ptr);
+	va_end(arg_ptr);
+	printf("\e[1;33m[WARN]:\e[0;33m %s\e[0m\n", output);
+}
 
 int main()
 {
-	const char *string = "jstring";
-	printf("%s\n", string);
-
+	void *string_memory = malloc(1024);
+	jstring_memory_activate(1024, string_memory);
+	jstring_load_logging_function(LOG_WARN);
+	jstring_load_logging_function(LOG_WARN);
 	return(0);
 }
